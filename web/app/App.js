744300232fc2332b32b65bbd2c642aa139431506
alessfm <alessandrofm@aluno.fapce.edu.br>
2022-01-16 19:52:44 -0300

    testing all features

--- web/app/App.js
+++ web/app/App.js
@@ -1,16 +1,30 @@
-import React, { Component } from "react";
-import "./App.css";
-import Logo from "./assets/img/logos/logo-half.png"
+import * as React from 'react';
+import { styled } from '@mui/material/styles';
+import Button from '@mui/material/Button';
+import IconButton from '@mui/material/IconButton';
+import PhotoCamera from '@mui/icons-material/PhotoCamera';
+import Stack from '@mui/material/Stack';
+import './App.css'
 
-class App extends Component{
-  render(){
-    return(
-      <div className="App">
-        <h1>Olá!!</h1>
-        <img src={Logo}></img>
-      </div>
-    );
-  }
+const Input = styled('input')({
+  display: 'none',
+});
+
+export default function UploadButtons() {
+  return (
+    <Stack direction="row" alignItems="center" spacing={2}>
+      <label htmlFor="contained-button-file">
+        <Input accept="image/*" id="contained-button-file" multiple type="file" />
+        <Button variant="contained" component="span">
+          Upload
+        </Button>
+      </label>
+      <label htmlFor="icon-button-file">
+        <Input accept="image/*" id="icon-button-file" type="file" />
+        <IconButton color="primary" aria-label="upload picture" component="span">
+          <PhotoCamera />
+        </IconButton>
+      </label>
+    </Stack>
+  );
 }
-
-export default App;
