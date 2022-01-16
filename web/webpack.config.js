1c5b20c136ab89e874baccf15e1c88ca8c4cc6e2
alessfm <alessandrofm@aluno.fapce.edu.br>
2022-01-16 17:04:37 -0300

    add plugin and start script

--- web/webpack.config.js
+++ web/webpack.config.js
@@ -1,34 +1,37 @@
-const path = require("path");
-const webpack = require("webpack");
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: "./src/index.js",
-  mode: "development",
+  mode: 'development',
+  entry: './app/index.js',
+  devtool: 'inline-source-map',
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  resolve: { 
+    extensions: ['*', '.js', '.jsx'] 
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './app/src/public/index.html'
+    })
+  ],
   module: {
     rules: [
       {
         test: /\.(js|jsx)$/,
-        exclude: /(node_modules|bower_components)/,
-        loader: "babel-loader",
-        options: { presets: ["@babel/preset-env"] }
+        loader: require.resolve('babel-loader'),
+        options: { presets: ['@babel/preset-env','@babel/preset-react'] }
       },
       {
         test: /\.css$/,
-        use: ["style-loader", "css-loader"]
-      }
+        use: ['style-loader', 'css-loader']
+      },
+      // {
+      //   test: /\.png|svg|jpg|gif$/,
+      //   use: ["file-loader"],
+      // },
     ]
-  },
-  resolve: { extensions: ["*", ".js", ".jsx"] },
-  output: {
-    path: path.resolve(__dirname, "dist/"),
-    publicPath: "/dist/",
-    filename: "bundle.js"
-  },
-  devServer: {
-    contentBase: path.join(__dirname, "public/"),
-    port: 3000,
-    publicPath: "http://localhost:3000/dist/",
-    hotOnly: true
-  },
-  plugins: [new webpack.HotModuleReplacementPlugin()]
+  }
 };
