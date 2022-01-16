5f8edaebeae8d5e6345f8028e70849419636e721
alessfm <alessandrofm@aluno.fapce.edu.br>
2022-01-16 17:31:01 -0300

    add Gardeno's logo

--- web/app/App.js
+++ web/app/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from "react";
 import "./App.css";
+import Logo from "./assets/img/logos/logo-half.png"
 
 class App extends Component{
   render(){
     return(
       <div className="App">
-        <h1> Hello, World! </h1>
+        <h1>Olá!!</h1>
+        <img src={Logo}></img>
       </div>
     );
   }
