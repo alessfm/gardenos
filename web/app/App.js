9c679c8adbe89e3c3d2f80f80d112e501f12547e
alessfm <alessandrofm@aluno.fapce.edu.br>
2022-04-03 16:00:33 -0300

    🌟 Add register page

--- web/app/App.js
+++ web/app/App.js
@@ -1,9 +1,8 @@
-// import Login from './src/public/pages/login/login';
-import SaveButton from './src/shared/components/buttons/btn-save/btn-save';
+import Login from './src/public/pages/login/login';
 import './App.css'
 
 export default function App() {
   return (
-    SaveButton()
+    Login()
   );
 }
