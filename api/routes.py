5b081eb3f42b73254693b6d0d61fa7bc148c3863
alessfm <alessandrofm@aluno.fapce.edu.br>
2022-04-30 16:15:04 -0300

    add CORS filter

--- api/routes.py
+++ api/routes.py
@@ -1,14 +1,27 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
+from fastapi.middleware.cors import CORSMiddleware
 
 
-class Usuario(BaseModel):
-    nome: str
+class User(BaseModel):
+    name: str
     email: str
-    senha: str
+    password: str
 
 
 app = FastAPI()
+
+origins = [
+    "http://localhost:8080",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.get("/")
@@ -17,5 +30,5 @@ def read_root():
 
 
 @app.post("/cadastro")
-async def cadastro(usuario: Usuario):
-    return usuario
+async def cadastro(user: User):
+    return user
